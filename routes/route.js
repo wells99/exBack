d9e80a64314cf971dd56c8a21c7688e0b691a726
Well <9.9wellington9.9@gmail.com>
2026-03-31 08:51:49 -0300

    usando json ao invez de csv para simplificar

--- routes/route.js
+++ routes/route.js
@@ -6,31 +6,87 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const router = Router();
+let meuResultado = [];
+
+const animesJson = path.join(__dirname, 'animes.json');
+
+meuResultado = JSON.parse(fs.readFileSync(animesJson, 'utf-8'));
+
 
 router.get('/animes', (req, res) => {
- const results = [];
- const csvPath = path.resolve(__dirname, 'top100animes.csv');
+  res.json(meuResultado);
 
-  fs.createReadStream(csvPath)
-    .pipe(csv({
-      // Converte colunas específicas para números
-      mapValues: ({ header, value }) => {
-        if (header === 'id' || header === 'votos') return Number(value);
-        return value;
-      }
-    }))
-    .on('data', (data) => results.push(data))
-   .on('end', () => {
-      res.json(results);
-    })
-    .on('error', (err) => {
-      res.status(500).send("Erro ao processar a base de dados");
-    });
 });
 
+router.post('/animes', (req, res) => {
+
+  const {nome, votos} = req.body;
+  const novoId = meuResultado.length + 1; 
+  console.log("NOVO ANIME ADICIONADO:");
+  const novoAnime = { id: novoId, nome, votos };
+
+  meuResultado.push(novoAnime); 
+  res.status(201).json(["Item adicionado", novoAnime]);
+
+  console.log("RESULTADOS ATUALIZADOS:");
+  console.log(meuResultado);
+});
+
+router.get('/animes/:id', (req, res) => {
+    const { id } = req.params;
+    const index = meuResultado.findIndex(item => item.id == id);
+    if (index !== -1) {
+        res.json(meuResultado[index]);
+    } else {
+        res.status(404).json({ mensagem: "Item não encontrado" });
+    }
+});
+
+router.put('/animes/:id', (req, res) => {
+    const { id } = req.params;
+    const index = meuResultado.findIndex(item => item.id == id);
+
+    if (index !== -1) {
+        meuResultado[index] = { ...meuResultado[index], ...req.body };
+        res.json({ mensagem: "Item atualizado!", item: meuResultado[index] });
+    } else {
+        res.status(404).json({ mensagem: "Item não encontrado" });
+    }
+});
+
+router.put('/animes/votos/:id', (req, res) => {
+    const { id } = req.params;
+    const index = meuResultado.findIndex(item => item.id == id);
+
+    if (index !== -1) {
+        meuResultado[index] = { ...meuResultado[index], votos: meuResultado[index].votos + 1 };
+        res.json({ mensagem: "Obrigado pelo su voto UP +1", item: meuResultado[index] });
+    } else {
+        res.status(404).json({ mensagem: "Item não encontrado" });
+    }
+});
+
+router.delete('/animes/:id', (req, res) => {
+  const { id } = req.params;
+    const tamanhoOriginal = meuResultado.length;
+    meuResultado = meuResultado.filter(item => item.id != id);
+
+    if (meuResultado.length < tamanhoOriginal) {
+        res.json({ mensagem: "Item removido com sucesso" });
+    } else {
+        res.status(404).json({ mensagem: "Item não encontrado" });
+    }
+});
+
+
+
+
+
+
+
 router.get('/filmes', (req, res) => {
- const results = [];
- const csvPath = path.resolve(__dirname, 'top100filmes.csv');
+  const results = [];
+  const csvPath = path.resolve(__dirname, 'top100filmes.csv');
 
   fs.createReadStream(csvPath)
     .pipe(csv({
@@ -41,7 +97,7 @@ router.get('/filmes', (req, res) => {
       }
     }))
     .on('data', (data) => results.push(data))
-   .on('end', () => {
+    .on('end', () => {
       res.json(results);
     })
     .on('error', (err) => {
